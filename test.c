8e9d26124145b4b14843b2eaa665ae1a2ecd485e
Edward J. Schwartz <edmcman@cmu.edu>
2025-12-04 21:40:09 +0000

    Fix example

--- test.c
+++ test.c
@@ -1,7 +1,6 @@
-typedef struct TYPE_2__ TYPE_1__;
-struct TYPE_2__ 
+void f()
 {
-    int value;
-    struct TYPE_2__* next;
-} ;
-typedef TYPE_1__* T;
+    T v = 0;
+    v->value = 42;
+    v->next = v;
+}
